31d54e406f8d5822922d4ac45f63db93cbf99369
Marni Ayyappa Swamy <ayyappa.marni@hotmail.com>
2022-04-29 20:13:35 +0200

    headrer and footer components added with rou config

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React, { FC } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
 import { HomePage } from "./pages/Home/HomePage";
 import { SearchPage } from "./pages/Search/SearchPage";
 
 export const App: FC = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="*" element={<HomePage />} />
-          <Route path="search" element={<SearchPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Header />
+      <Children />
+      <Footer />
+    </Router>
+  );
+};
+
+const Children = () => {
+  return (
+    <Routes>
+      <Route path="*" element={<HomePage />} />
+      <Route path="search" element={<SearchPage />} />
+    </Routes>
   );
 };
