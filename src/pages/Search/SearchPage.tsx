f5638d0a6404b488a10f8e1a20419ffc361cb511
Marni Ayyappa Swamy <ayyappa.marni@hotmail.com>
2022-05-01 19:18:25 +0200

    toggle menu in mobile view

--- src/pages/Search/SearchPage.tsx
+++ src/pages/Search/SearchPage.tsx
@@ -1,38 +1,61 @@
-import React, { FC } from "react";
+import "./SearchPage.scss";
+import React, { FC, useState } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import { SideBar } from "../../components/SideBar/SideBar";
 import { SearchPackages } from "../../components/SearchPackages/SearchPackages";
+import { Box } from "@mui/material";
 
 export const SearchPage: FC = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggelMenuHandler = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <Container maxWidth="lg" className="search-container">
-      <Grid container rowSpacing={1}>
-        <Grid
-          item
-          xs={0}
-          sm={0}
-          md={2}
-          lg={2}
-          sx={{
-            display: { xs: "none", sm: "none", md: "block" },
-          }}
-        >
-          <SideBar />
+    <React.Fragment>
+      <Box
+        className="menu-button"
+        sx={{
+          display: { xs: "block", sm: "block", md: "none" },
+        }}
+        onClick={() => toggelMenuHandler()}
+      >
+        Menu
+      </Box>
+      <Container maxWidth="lg" className="search-container">
+        <Grid container rowSpacing={1}>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={2}
+            lg={2}
+            sx={{
+              display: {
+                xs: showMenu ? "block" : "none",
+                sm: "none",
+                md: "block",
+              },
+            }}
+          >
+            <SideBar />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={10}
+            lg={10}
+            sx={{
+              display: { xs: "block", sm: "block", md: "block" },
+            }}
+          >
+            <SearchPackages />
+          </Grid>
         </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          md={10}
-          lg={10}
-          sx={{
-            display: { xs: "block", sm: "block", md: "block" },
-          }}
-        >
-          <SearchPackages />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </React.Fragment>
   );
 };
