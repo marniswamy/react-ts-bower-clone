81fb93d8a477a9fbbd05aaffcef9e95aae3f897b
Marni Ayyappa Swamy <ayyappa.marni@hotmail.com>
2022-05-02 23:25:19 +0200

    test case and doc blocks

--- src/helpers/helper.test.ts
+++ src/helpers/helper.test.ts
@@ -1,8 +1,13 @@
-import { getOwnerNameHelper } from "./helpers";
+import { getOwnerNameHelper, getPageTitle } from "./helpers";
 
 describe("getOwnerNameHelper", () => {
   it("it should return 4th element from an array", () => {
     const mockData = "https://github.com/FortAwesome/Font-Awesome.git";
     expect(getOwnerNameHelper(mockData)).toEqual("FortAwesome");
+  });
+
+  it("it should return Leaflet from this url", () => {
+    const mockData = "https://github.com/Leaflet/Leaflet";
+    expect(getOwnerNameHelper(mockData)).toEqual("Leaflet");
   });
 });
--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,5 +1,34 @@
 import { SORTING_VALUES } from "../constants/SORTING_VALUES";
 
+/**
+ * getOwnerNameHelper is used to get the name from the github url
+ *
+ * @param repoUrl - github url as param
+ * @returns the owner name of the repo
+ */
+export const getOwnerNameHelper = (repoUrl: string) => {
+  return repoUrl.split("/")[3];
+};
+
+/**
+ * getPageTitle helper is used to get the header title
+ * dynamically based on the page we are in
+ *
+ * @returns the string
+ */
+export const getPageTitle = () => {
+  const { pathname } = window.location;
+  const onSearchPage = pathname.includes("/search");
+  return onSearchPage ? "Bower Search" : "Bower Packages";
+};
+
+/**
+ * compare method is used to compare the values for sorting method
+ *
+ * @param a is the prev value of starts in this case
+ * @param b is the next value of starts in this case
+ * @returns the number
+ */
 export const compare = (a: any, b: any) => {
   const prev = a.stars;
   const next = b.stars;
@@ -13,6 +42,14 @@ export const compare = (a: any, b: any) => {
   return 0;
 };
 
+/**
+ * sortDataBySortType method is used to sort the arraz of data
+ * based on flag ASC or DSC
+ *
+ * @param data - array of objects as param
+ * @param sortType - tzpe of sort ASC or DSC
+ * @returns - retuns the arrz with sorted by given param
+ */
 export const sortDataBySortType = (data = [], sortType: string) => {
   if (sortType === SORTING_VALUES.ASCENDING) {
     return data.sort(compare);
@@ -23,14 +60,4 @@ export const sortDataBySortType = (data = [], sortType: string) => {
   }
 
   return data;
-};
-
-export const getOwnerNameHelper = (repoUrl: string) => {
-  return repoUrl.split("/")[3];
-};
-
-export const getPageTitle = () => {
-  const { pathname } = window.location;
-  const onSearchPage = pathname.includes("/search");
-  return onSearchPage ? "Bower Search" : "Bower Packages";
 };
